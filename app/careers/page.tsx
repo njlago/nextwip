04d28ab172469884c0d79501a9c187f1c01e3691
njlago <nicklago7@yahoo.com>
2024-02-29 13:16:45 -0500

    work in progress

---
+++ app/careers/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function page() {
+  return (
+    <div className="flex items-center justify-center h-screen text-white bg-black">
+      <div className="text-center">
+        <Link href="/">
+          <button>Careers.</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
